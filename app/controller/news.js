c9b9d4de6223dca95dcd19ea5cd9a8bd56ec1de0
hatu <fomenyesu@gmail.com>
2017-04-10 11:14:59 +0800

    Update news.js

--- app/controller/news.js
+++ app/controller/news.js
@@ -2,20 +2,18 @@
 const createRule = {name: 'string', pass: 'string', };
 const updateByIdRule = {id: 'int', name: 'string', pass: 'string', }
 
-// 1 获取用户列表，分页，每页几个
+// 1 获取文章列表，分页，每页几个
 exports.index = function* () {
   console.log(this.params);
-  // 调用 service 创建一个 topic
   const result = yield this.service.news.index(this.params);
 
   // 设置响应体和状态码
   this.body = result;
   this.status = 201;
 };
-// 2 根据ID获取用户信息
+// 2 根据ID获取文章信息
 exports.show = function* () {
   console.log(this.params);
-  // 调用 service 创建一个 topic
   const result = yield this.service.news.show(this.params);
 
   // 设置响应体和状态码
@@ -26,7 +24,6 @@ exports.show = function* () {
 exports.create = function* () {
   console.log(this.params);
   this.validate(createRule);
-  // 调用 service 创建一个 topic
   const result = yield this.service.news.create(this.params);
 
   // 设置响应体和状态码
@@ -37,7 +34,6 @@ exports.create = function* () {
 exports.update = function* () {
   console.log(this.params);
   this.validate(updateByIdRule);
-  // 调用 service 创建一个 topic
   const result = yield this.service.news.update(this.params);
 
   // 设置响应体和状态码
